9c45936c3ae700e0c0d9e5b3ed2cbef2979e76eb
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-04-18 23:04:22 +0900

    fix

--- MyInvertedIndex.java
+++ MyInvertedIndex.java
@@ -1,43 +1,65 @@
-import java.io.IOException;
-import java.util.StringTokenizer;
-import java.util.concurrent.ConcurrentHashMap;
+import java.io.*;
+import java.util.*;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.mapreduce.lib.output.*;
 
-import org.w3c.dom.Text;
-
+// bible inverted index로 변환
 public class MyInvertedIndex{
     
     public static class MyMapper extends Mapper<Object, Text, Text, Text>{
+        //입력의 key, value, 출력의 key, value
         private Text word = new Text();
+        // 단어를 담는 용도
+        private Text docInfo = new Text();
+        // 제목이랑 챕터:벌스 담을 용도
         public void map(Object key, Text value, Context context)
         throws IOException, InterruptedException{
+            //text value : 한줄단위로 들어오는 map함수의 입력
+            // Context : 결과를 Hadoop으로 전달하기 위한 연결고리
             String line = value.toString();
-            String DocId = line.substring(0, value.toString().indexOf("\t"));
-            String value_raw = line.substring(value.toString().indexOf("\t")+1);
-            String match = "[^\uAC00-\uD7A3xfea-zA-Z\\s]";
+            // Hadoop에서 사용하는 Text 자료구조를 String으로 변환
+            String DocId_doc_title = line.substring(0, value.toString().indexOf("\r"));
+            //처음부터 \n 나올때까지 -> 제목
+            String value_raw = line.substring(value.toString().indexOf("\r")+1);
+            // \n 나온 이후 => 본문
+            String chapter_verse = value_raw.substring(0, value_raw.indexOf(' '));
+            // DocId_doc_title과 chapter_verse 결합
+            String combinedInfo = DocId_doc_title + "\t" + chapter_verse;
+            docInfo.set(combinedInfo);
+
+            String match = "[^\uAC00-\uD7A3xfea-zA-Z\\s]";  // 특수문자 제거
             StringTokenizer st = new StringTokenizer(value_raw, " '-");
             while(st.hasMoreTokens()){
                 word.set(st.nextToken().replaceAll(match, "").toLowerCase());
                 if(word.toString()!=""&&!word.toString().isEmpty()){
-                    context.write(word, new Text(DocId));
+                    context.write(word, new Text(combinedInfo));
                 }
             }
         }
     }
 
     public static class MyReducer extends Reducer<Text, Text, Text, Text>{
-        protected voic reduce(Text key, Iterable<Text> values, Context context)
+        protected void reduce(Text key, Iterable<Text> values, Context context)
         throws IOException, InterruptedException{
-            HashMap<String, Integer> map = new HashMap<String, Integer>();
-            for(Text val: values){
-                if(map.containsKey(val.toString())){
-                    map.put(val.toString(), map.get(val.toString())+1);
-                }else{
-                    map.put(val.toString(), 1);
-                }
-            }
+            // HashMap<String, Integer> map = new HashMap<String, Integer>();
+            // for(Text val: values){  
+            //     if(map.containsKey(val.toString())){
+            //         map.put(val.toString(), map.get(val.toString())+1); //빈도수 +1
+            //     }else{
+            //         map.put(val.toString(), 1);
+            //     }
+            // }
             StringBuilder docValueList = new StringBuilder();
-            for(String docID:map.keySet()){
-                docValueList.append(docID+":"+map.get(doxID)+" ");
+            // for(String docID:map.keySet()){
+            //     docValueList.append(docID+":"+map.get(docID)+" ");
+            // }
+            for(String comb:values){
+                String[] parts = comb.split("\t");
+                docValueList.append(parts[0]+":"+parts[1]+" ");
             }
             context.write(key, new Text(docValueList.toString()));
         }
@@ -54,7 +76,7 @@ public class MyInvertedIndex{
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.addOutputPath(job, new Path(args[1]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
         job.waitForCompletion(true);
     };
 
