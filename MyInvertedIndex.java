29892fff37a200b1f0f81e65db45915da256e63b
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-04-18 12:56:33 +0900

    wow

--- MyInvertedIndex.java
+++ MyInvertedIndex.java
@@ -5,6 +5,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.w3c.dom.Text;
 
 public class MyInvertedIndex{
+    
     public static class MyMapper extends Mapper<Object, Text, Text, Text>{
         private Text word = new Text();
         public void map(Object key, Text value, Context context)
